dc27c3a71215cfbdfcf4fa9bc2fed0b32e58023a
Victor Bertoldo <eu_viktor@hotmail.com>
2020-05-07 15:15:02 -0300

    .

---
+++ webscraping/webscraping.py
@@ -0,0 +1,7 @@
+﻿ # 1. Pegar o conteudo HTML a partir da URL
+# 2. Parsear o conteudo HTML - BeaultifulSoup
+# 3. Estruturar conteúdo em Data frame - Pandas
+# 4. Transformar os Dados em um dicionario de dados
+# 5. Converter e salvar o conteudo em JSON
+
+PIP:
